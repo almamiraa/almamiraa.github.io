25c39d018be09a758144314e571616a297133a22
almamiraa <almaaamirad@gmail.com>
2023-11-02 13:12:09 +0700

    Deploy

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="APP">
+    <CalculatorApp> </CalculatorApp>
+  </div>
+</template>
+
+<script>
+import CalculatorApp from './components/CalculatorApp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CalculatorApp
+}
+}
+</script>
+
+<style>
+  body {
+    background: #5888bb !important;
+  }
+</style>
